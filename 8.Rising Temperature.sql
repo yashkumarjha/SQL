ba9b0195a0a5ef2eacb575fb68512025d0597e68
yashkumarjha <55932900+yashkumarjha@users.noreply.github.com>
2024-02-25 23:00:48 +0530

    Create 8.Rising Temperature.sql

---
+++ 8.Rising Temperature.sql
@@ -0,0 +1,5 @@
+-- https://leetcode.com/problems/rising-temperature/description/
+
+SELECT A.ID FROM WEATHER A, WEATHER B
+WHERE DATEDIFF(A.RECORDDATE,B.RECORDDATE)=1
+AND A.TEMPERATURE > B.TEMPERATURE;
