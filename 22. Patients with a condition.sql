a92911b96ebf6ba694b25385f332bb88e71251f9
yashkumarjha <55932900+yashkumarjha@users.noreply.github.com>
2024-03-25 09:19:53 +0530

    Create 22. Patients with a condition.sql

---
+++ 22. Patients with a condition.sql
@@ -0,0 +1,5 @@
+-- https://leetcode.com/problems/patients-with-a-condition/
+
+SELECT PATIENT_ID, PATIENT_NAME, CONDITIONS
+FROM PATIENTS
+WHERE CONDITIONS LIKE '% DIAB1%' OR CONDITIONS LIKE 'DIAB1%';
